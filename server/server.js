82579e0c2523466295bb71cd955ab755f5175572
Sourabh <sourabh.khera@tothenew.com>
2020-06-18 02:11:50 +0530

    sso done

--- server/server.js
+++ server/server.js
@@ -1,38 +1,100 @@
 const express = require('express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
-const path = require('path');
+const cors = require('cors');
+const ejs = require('ejs');
+const session = require('express-session');
+const jwt = require('jsonwebtoken');
+const redis = require('redis');
+const client = redis.createClient();
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const crypto = require('crypto');
 
-const initExpress = require('./init/expressInit');
-const initRoutes = require('./init/routesInit');
-const { options } = require('./utils');
-global.logger = require('./configurations/logger');
+client.on("error", (error) => {
+  console.log(error, "redis error");
+});
 
 const app = express();
 
-/*
- * express settings
- */
+const dbString = 'mongodb://localhost/session_db';
+const connection = mongoose.createConnection(dbString);
+const sessionStore = new MongoStore({
+  mongooseConnection: connection,
+  collection: 'sessions_sso'
+});
 
-initExpress(app);
+app.set('view engine', ejs);
+app.set('views', './server/views');
 
-/*
- * server application routes
- *
- */
+const options = {
+  credentials: true
+}
+app.use(cors(options));
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  name: 'sso_authServer',
+  resave: false,
+  saveUninitialized: false,
+  secret: '12345',
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
 
-initRoutes(app);
+const login = async (req, res, next) => {
+  const { serviceURL } = req.query;
+  app.locals.ssoTokenKey = `ssoToken${Math.random()}`;
+  let ssoToken;
+  if (!req.session.user && !serviceURL) {
+    res.redirect('/')
+  }
+  if (req.session.user && serviceURL) {
+    let ssoToken = crypto.randomBytes(25).toString('hex');
+    client.set(app.locals.ssoTokenKey, ssoToken);
+    return res.redirect(`${serviceURL}/ssoRedirect?ssoToken=${ssoToken}`);    
+  }
+  app.locals.serviceURL = serviceURL;
+  next();
+}
 
-const swaggerDocs = swaggerJsDoc(options);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-// All undefined asset or api routes should return a 404
-app.route('*')
-  .get((req, res) => res.status(404).send({message:'not found!!'}))
-  .post((req, res) => res.status(404).send({message:'not found!!'}))
-  .put((req, res) => res.status(404).send({message:'not found!!'}))
-  .delete((req, res) => res.status(404).send({message:'not found!!'}));
+const verifySSOToken = (req, res, next) => {
+  const { ssoToken } = req.query;
+  client.get(app.locals.ssoTokenKey, (err, ssoKey) => {
+    if (err) {
+      console.log(err, "error while getting key from redis");
+    }
+    if (!ssoToken || ssoToken !== ssoKey || !ssoKey) {
+      return res.status(401).send({ message: 'Unauthorized' });
+    }
+    const userClaim = req.session.user;
+    const token = jwt.sign(userClaim, 'abcde@12345', { expiresIn: '1d' });
+    client.del(app.locals.ssoTokenKey, (err, data) => {
+      if (err) {
+        console.log(err, 'error while deleting key from redis');
+      }
+      console.log('key deleted', data);
+      res.status(200).send({ token });
+    });
+  });
+}
 
+app.use('/verifySSO', verifySSOToken);
 
-app.listen(app.get('port'));
+app.get('/simplesso/login', login, (req, res) => {
+  res.render('login.ejs')
+});
 
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'sourabh' && password === '12345') {
+    req.session.user = { username };
+    let ssoToken = crypto.randomBytes(25).toString('hex');
+    client.set(app.locals.ssoTokenKey, ssoToken);
+    return res.redirect(`${app.locals.serviceURL}/ssoRedirect?ssoToken=${ssoToken}`);          
+  } else {
+    return res.status(404).send({ message: 'Invalid username or password' });
+  }
+});
+
+app.listen(4000, () => console.log('server listening on port 4000'));
 module.exports = app;
